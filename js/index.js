da595a0487790d622dfca69eb27b8630c8a101b5
Tarkanii Artur <arturmandragon@gmail.com>
2024-11-16 14:23:56 +0000

    WS-10, added portfolio showcases

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 import { menuButtonHandler } from "./menuButton.js";
+import { filtersHandler } from "./portfolioFilters.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     menuButtonHandler();
+    filtersHandler();
 })
